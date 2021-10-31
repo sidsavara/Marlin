3568fe93758d4f3956f9ca16da0e0f66f6072daa
Sid Savara <sid@sidsavara.com>
2021-10-31 09:20:04 -0500

    Fixed probe offsets fo CR Touch & made 49 pt grid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1181,7 +1181,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -9, -1.25 }
+#define NOZZLE_TO_PROBE_OFFSET { -47, -5, -1.6 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1605,7 +1605,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
